311560ef37852324045828e6d7667cd55d95fec8
Mathieu Bourlet <mathieu.bourlet@assoconnect.com>
2020-04-30 18:25:18 +0200

    Breaking the circular dependency of package

--- src/AssoConnectValidatorBundle.php
+++ src/AssoConnectValidatorBundle.php
@@ -2,8 +2,6 @@
 
 namespace AssoConnect\ValidatorBundle;
 
-use AssoConnect\DoctrineValidatorBundle\DependencyInjection\AssoConnectDoctrineValidatorExtension;
-use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class AssoConnectValidatorBundle extends Bundle
