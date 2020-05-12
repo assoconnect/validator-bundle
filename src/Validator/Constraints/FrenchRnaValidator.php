4b53733109966e8b21e38af1ea36f87c86927531
Florian Guimier <florian.guimier@assoconnect.com>
2020-05-12 19:35:16 +0200

    Handle lower/upper case letters in the RNA validator

--- src/Validator/Constraints/FrenchRnaValidator.php
+++ src/Validator/Constraints/FrenchRnaValidator.php
@@ -16,16 +16,40 @@ class FrenchRnaValidator extends ConstraintValidator
     {
         // Source : https://www.data.gouv.fr/fr/datasets/repertoire-national-des-associations/
         // the "jgmrt" is a specific letter for the DOM-TOM
-        if (preg_match('/(^w\d[\djgmrt]\d{7}$)|(^\d[\djgmrt]\d[ps]\d{10}$)/i', $value) !== 1) {
-            $this->context->buildViolation($constraint->message)
-                ->setParameter('{{ value }}', $this->formatValue($value))
-                ->setCode(FrenchRna::INVALID_FORMAT_ERROR)
-                ->addViolation();
+        if ($this->isValidFrenchRna($value, true) ||
+            $this->isValidFrenchRna($value, false)
+        ) {
+            return;
         }
+
+        $this->context->buildViolation($constraint->message)
+            ->setParameter('{{ value }}', $this->formatValue($value))
+            ->setCode(FrenchRna::INVALID_FORMAT_ERROR)
+            ->addViolation();
     }
 
     public function getSupportedConstraint(): string
     {
         return FrenchRna::class;
+    }
+
+    private function isValidFrenchRna($value, bool $checkForCapitalLetters): bool
+    {
+        // The letters must be all uppercase or all lowercase but not a mix of both types
+        if ($checkForCapitalLetters) {
+            $firstCharacters           = 'W';
+            $specificLettersFirstPart  = 'JGMRT';
+            $specificLettersSecondPart = 'PS';
+        } else {
+            $firstCharacters           = 'w';
+            $specificLettersFirstPart  = 'jgmrt';
+            $specificLettersSecondPart = 'ps';
+        }
+
+        return (!(preg_match(
+                '/(^' . $firstCharacters . '\d[\d' . $specificLettersFirstPart .
+                ']\d{7}$)|(^\d[\d' . $specificLettersFirstPart . ']\d[' . $specificLettersSecondPart . ']\d{10}$)/',
+                $value
+            ) !== 1));
     }
 }
