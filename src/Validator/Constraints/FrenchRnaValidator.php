0d0bafdddc3129fc12b9230a1931c9a375f9830a
Florian Guimier <florian.guimier@assoconnect.com>
2020-05-14 19:23:42 +0200

    Only use PSR-12 to avoid rules conflicts

--- src/Validator/Constraints/FrenchRnaValidator.php
+++ src/Validator/Constraints/FrenchRnaValidator.php
@@ -20,7 +20,8 @@ class FrenchRnaValidator extends ConstraintValidator
 
         // Source : https://www.data.gouv.fr/fr/datasets/repertoire-national-des-associations/
         // the "ABGMCRSTJ" is a specific letter for the DOM-TOM and Corse
-        if (!is_string($value) ||
+        if (
+            !is_string($value) ||
             preg_match('/(^W\d[\dABGMCRSTJ]\d{7}$)|(^\d[\dABGMRT]\d[PS]((02[BA])|(\d{3}))\d{7}$)/', $value) !== 1
         ) {
             $this->context->buildViolation($constraint->message)
