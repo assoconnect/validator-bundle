71e4b384ad80ddcc793fbebe1ced31e50c0259f7
Julien <julien.waguet@assoconnect.com>
2021-09-07 16:01:00 +0200

    Add new validator

---
+++ src/Validator/Constraints/EmployerIdentificationNumber.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AssoConnect\ValidatorBundle\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * @Annotation
+ */
+class EmployerIdentificationNumber extends Constraint
+{
+    public const WRONG_FORMAT_ERROR = '890163a8-f289-4c6b-aa2b-73890f93ca5c';
+    public $message = 'The value {{ value }} is not a valid employer identification number.';
+}
---
+++ src/Validator/Constraints/EmployerIdentificationNumberValidator.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AssoConnect\ValidatorBundle\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Component\Validator\Exception\UnexpectedTypeException;
+use Symfony\Component\Validator\Exception\UnexpectedValueException;
+
+class EmployerIdentificationNumberValidator extends ConstraintValidator
+{
+    public function validate($value, Constraint $constraint)
+    {
+
+        if (!$constraint instanceof EmployerIdentificationNumber) {
+            throw new UnexpectedTypeException($constraint, __NAMESPACE__ . '\EmployerIdentificationNumber');
+        }
+
+        if (null === $value || '' === $value) {
+            return;
+        }
+
+        if (!is_string($value)) {
+            throw new UnexpectedValueException($value, 'string');
+        }
+
+        if (!preg_match('/^[1-9]\d?-\d{7}$/', $value, $matches)) {
+            $this->context->buildViolation($constraint->message)
+                ->setParameter('{{ value }}', $this->formatValue($value))
+                ->setCode(EmployerIdentificationNumber::WRONG_FORMAT_ERROR)
+                ->addViolation();
+        }
+    }
+
+    public function getSupportedConstraint(): string
+    {
+        return EmployerIdentificationNumber::class;
+    }
+}
