4889c4eba178de47a83aefc1bd3d44a60ae23d02
Florian Guimier <florian.guimier@assoconnect.com>
2024-06-03 11:55:36 +0200

    FIx

--- tests/Validator/ConstraintsSetProvider/Field/UsSocialSecurityNumberProviderTest.php
+++ tests/Validator/ConstraintsSetProvider/Field/UsSocialSecurityNumberProviderTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace AssoConnect\ValidatorBundle\Tests\Validator\ConstraintsSetProvider\Field;
 
 use AssoConnect\ValidatorBundle\Test\FieldConstraintsSetProviderTestCase;
+use AssoConnect\ValidatorBundle\Validator\Constraints\UsSocialSecurityNumber;
 use AssoConnect\ValidatorBundle\Validator\ConstraintsSetProvider\Field\FieldConstraintsSetProviderInterface;
 use AssoConnect\ValidatorBundle\Validator\ConstraintsSetProvider\Field\UsSocialSecurityNumberProvider;
 
@@ -17,9 +18,6 @@ class UsSocialSecurityNumberProviderTest extends FieldConstraintsSetProviderTest
 
     public function getConstraintsForTypeProvider(): iterable
     {
-        yield [
-            ['type' => 'usSocialSecurityNumber'],
-            [new UsSocialSecurityNumberProvider()],
-        ];
+        yield [['type' => 'usSocialSecurityNumber'], [new UsSocialSecurityNumber()]];
     }
 }
