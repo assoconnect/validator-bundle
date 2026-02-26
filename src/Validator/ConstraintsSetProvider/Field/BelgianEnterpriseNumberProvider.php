48d3dc0d6a5ac3ab9fc3959f6e626bd22845cf76
Florian Guimier <florian.guimier@assoconnect.com>
2026-02-26 16:24:01 +0100

    FIx

--- src/Validator/ConstraintsSetProvider/Field/BelgianEnterpriseNumberProvider.php
+++ src/Validator/ConstraintsSetProvider/Field/BelgianEnterpriseNumberProvider.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace AssoConnect\ValidatorBundle\Validator\ConstraintsSetProvider\Field;
 
+use AssoConnect\DoctrineTypesBundle\Doctrine\DBAL\Types\BelgianEnterpriseNumberType;
 use AssoConnect\ValidatorBundle\Validator\Constraints\BelgianEnterpriseNumber;
 
 class BelgianEnterpriseNumberProvider implements FieldConstraintsSetProviderInterface
